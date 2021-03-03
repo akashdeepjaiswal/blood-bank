bd98e5c4487d2226e51b97130b1e6ebc31a41815
Akashdeep Jaiswal <akashdeep0897@gmail.com>
2021-03-04 00:43:23 +0530

    aws working

--- sql_conn.js
+++ sql_conn.js
@@ -1,11 +1,13 @@
 //sql
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  user     : 'root',
-  password : '',
-  database : "blood_bank_db"
+  host     : 'blood-bank.cfa33wsn7pjo.us-east-1.rds.amazonaws.com',
+  port : '3306',
+  user     : 'admin',
+  password : 'Akash01!',
+  database: 'blood_bank_db'
 });
+
 
 connection.connect(function(err) {
   if (err) {
@@ -16,4 +18,7 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
+
 module.exports = connection;
+
+
